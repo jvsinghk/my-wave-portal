404220d3e56217ddc9c9ff835ff184f1256a3ac0
jvsinghk <jvsinghk@gmail.com>
2022-10-24 23:06:28 +0530

    feat: add waveportal smart contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
